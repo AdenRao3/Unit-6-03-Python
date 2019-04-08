4aa548973e79bcdb9ab74c3db12360f5a4d41d52
AdenRao3 <47573320+AdenRao3@users.noreply.github.com>
2019-04-07 20:29:18 -0400

    Add files via upload

---
+++ movie.py
@@ -0,0 +1,11 @@
+import math
+
+myAge = int(input("Type your age: "))
+
+if myAge >= (17):
+		print("You can watch R Rated Movies")
+elif myAge >= (13):
+		print("You can watch PG-13 Rated Movies")
+elif myAge >= (1):
+		print("You can watch G Rated Movies")
+
